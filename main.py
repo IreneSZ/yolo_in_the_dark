8dd0ffbf183df047f8c5b822d047ce8f76bdcf90
irenesz <ivyzsy@gmail.com>
2019-09-29 20:42:00 -0400

    initial commit

--- main.py
+++ main.py
@@ -8,18 +8,26 @@ import time
 import datetime
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
-parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
-parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
-parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
-parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
-parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
-parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
-parser.add_argument("--compute_map", default=False, help="if True computes mAP every tenth batch")
-parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
-opt = parser.parse_args()
-print(opt)
+from train import Trainer
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=5, help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=1, help="size of each image batch")
+    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
+    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
+    parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
+    parser.add_argument("--compute_map", default=False, help="if True computes mAP every tenth batch")
+    parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    parser.add_argument("--ID_txt", default='./data/id.txt', help="IDs for images and labels")
+    # opt = parser.parse_known_args()[0]
+    opt = parser.parse_args()
+
+    trainer = Trainer(opt.epochs, opt.ID_txt, opt.batch_size, opt.n_cpu)
+    trainer.train()
+    
